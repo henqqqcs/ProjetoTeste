b839b3b7e86e3a8282044a2c7069f41b58468358
Henrique Barreto <>
2016-07-06 09:57:43 -0300

    Second commit

---
+++ src/testegithub/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testegithub;
+
+/**
+ *
+ * @author infosolo
+ */
+public class Hello {
+    
+    public void teste() {
+        System.out.println("testegithub.Hello.teste()");
+    }
+}
